5a2421fbf8b228df936e2dc08cd05021f41ac503
liammews <liam.mews@outlook.com>
2021-03-03 18:39:35 +0000

    Attempt to fix delpoy css issue

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: [],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     colors: {
