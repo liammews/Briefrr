061f2623c5e479153f28b20eb97a49f858161a7e
liammews <liam.mews@outlook.com>
2021-03-13 22:44:09 +0000

    Fixed Some Internal Links

--- web/src/components/graphic design/gd-hero.js
+++ web/src/components/graphic design/gd-hero.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Hero = () => {
     return(
         <div className="flex flex-col">
         
-        <div><a href="/inbox" className="no-underline"><p className="text-blue-500">← Back To Inbox</p></a></div>
+        <div><Link to="/inbox" className="no-underline"><p className="text-blue-500">← Back To Inbox</p></Link></div>
         <h1 className="text-4xl lg:text-5xl dark:text-white">Graphic Design</h1>
         
         <hr className="dark:border-gray-700 my-8"></hr>
--- web/src/components/resources/iconshero.js
+++ web/src/components/resources/iconshero.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Hero = () => {
     return(
         <div className="flex flex-col">
         
-        <div><a href="/resources" className="no-underline"><p className="text-blue-500 dark:text-green-300">← Back to all resources</p></a></div>
+        <div><Link to="/resources"><p className="text-blue-500 dark:text-green-300">← Back to all resources</p></Link></div>
         <h1 className="text-4xl lg:text-5xl dark:text-white">Icons</h1>
 
         <hr className="dark:border-darkgray-700 my-8"></hr>
